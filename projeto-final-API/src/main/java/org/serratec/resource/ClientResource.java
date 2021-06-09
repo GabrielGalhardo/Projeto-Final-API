5addd7c1f819f4afcdf601a7062f141943e3c580
GuilhermeSantosmain <guilhermesantosmain@gmail.com>
2021-06-09 14:25:32 -0300

    validaçao de cpf

--- projeto-final-API/src/main/java/org/serratec/resource/ClientResource.java
+++ projeto-final-API/src/main/java/org/serratec/resource/ClientResource.java
@@ -22,6 +22,25 @@ public class ClientResource {
 	@Autowired
 	ClientRepository clientRepository;
 	
+	private static final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+	
+	   private static int calcularDigito(String str, int[] peso) {
+	      int soma = 0;
+	      for (int indice=str.length()-1, digito; indice >= 0; indice-- ) {
+	         digito = Integer.parseInt(str.substring(indice,indice+1));
+	         soma += digito*peso[peso.length-str.length()+indice];
+	      }
+	      soma = 11 - soma % 11;
+	      return soma > 9 ? 0 : soma;
+	   }
+
+	   public static boolean isValidCpf(String cpf) {
+	      if ((cpf==null) || (cpf.length()!=11)) return false;
+
+	      Integer digito1 = calcularDigito(cpf.substring(0,9), pesoCPF);
+	      Integer digito2 = calcularDigito(cpf.substring(0,9) + digito1, pesoCPF);
+	      return cpf.equals(cpf.substring(0,9) + digito1.toString() + digito2.toString());
+	   }
 	
 	@PostMapping("/client")
     public ResponseEntity<?> post(@Validated @RequestBody ClientCadastroDTO dto) {
@@ -29,8 +48,8 @@ public class ClientResource {
 		Client cliente = dto.toClient(clientRepository);
         
 		try {
-        	if(clientRepository.existsByCpf(cliente.getCpf()))
-        		throw new ClientException("CPF já cadastrado");
+        	if(clientRepository.existsByCpf(cliente.getCpf()) || !isValidCpf(cliente.getCpf()))
+        		throw new ClientException("CPF invalido ou ja cadastrado");
         	
         	if(clientRepository.existsByUsername(cliente.getUsername()))
         		throw new ClientException("Username já cadastrado");
