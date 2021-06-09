9119bc62b9b47114936d0fa623d63377ca6565b4
GuilhermeSantosmain <guilhermesantosmain@gmail.com>
2021-06-09 15:42:55 -0300

    verificacao de cep

--- projeto-final-API/src/main/java/org/serratec/resource/EnderecoResource.java
+++ projeto-final-API/src/main/java/org/serratec/resource/EnderecoResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.serratec.dto.endereco.EnderecoCompletoDTO;
+import org.serratec.dto.endereco.EnderecoViaCepDTO;
 import org.serratec.models.Endereco;
 import org.serratec.repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class EnderecoResource {
@@ -41,6 +43,15 @@ public class EnderecoResource {
 	
 	@PostMapping("/endereco")
 	public ResponseEntity<?> postEndereco(@Validated @RequestBody Endereco endereco) {
+		
+		String uri = "https://viacep.com.br/ws/" + endereco.getCep() + "/json/";
+
+	    RestTemplate rest = new RestTemplate();
+	    EnderecoViaCepDTO viaCep = rest.getForObject(uri, EnderecoViaCepDTO.class);
+	    
+	    if(viaCep.getUf() == null)
+	    	return new ResponseEntity<>("Cep invalido", HttpStatus.BAD_REQUEST);
+		
 		repository.save(endereco);
 		
 		return new ResponseEntity<>("Endereco cadastrado com sucesso!", HttpStatus.OK);
